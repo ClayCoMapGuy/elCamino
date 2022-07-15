58de59c6cb19e7ed6b7b86eb12a783e3b8100a8d
ClayCoMapGuy <robert.zimmerman@claytoncountyga.gov>
2022-07-15 14:27:11 -0400

    Update App.js
    Removed default stuff

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ export function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Just some words
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
