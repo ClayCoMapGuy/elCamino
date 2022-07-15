4c96607816d7d7abe1a530a22620628b7a75ea65
ClayCoMapGuy <robert.zimmerman@claytoncountyga.gov>
2022-07-15 14:41:42 -0400

    File restructure
    Testing making changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
 
-export function App() {
+export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Some placeholder words
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
