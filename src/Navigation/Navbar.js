7af0a7fdf0eff9e4f901a003b45844b8df5f7ffc
ClayCoMapGuy <robert.zimmerman@claytoncountyga.gov>
2022-07-15 14:25:32 -0400

    Create Navbar.js

---
+++ src/Navigation/Navbar.js
@@ -0,0 +1,17 @@
+import '../App.css';
+import clayconn from '../Graphics/clayconn.svg';
+
+export function Navbar() {
+  return (
+    <div className="Navbar">
+      <div>
+        <a href='https://claytoncountyga.gov'>
+          <img src={clayconn} alt='' />
+        </a>
+      </div>
+      <div>
+        <p>Clayton County Permitting</p>
+      </div>
+    </div>
+  )
+}
