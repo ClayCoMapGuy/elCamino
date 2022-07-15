7249ef35d14511f33d4676c2bde9ca8aea068d6a
ClayCoMapGuy <robert.zimmerman@claytoncountyga.gov>
2022-07-15 14:22:34 -0400

    Organized File Structure
    But generally just doing this as a test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 //import logo from './logo.svg';
 import './App.css';
-import clayconn from './clayconn.svg'
 
 export function App() {
   return (
@@ -21,22 +20,3 @@ export function App() {
     </div>
   );
 }
-
-export function Navbar() {
-  return (
-    <div className="Navbar">
-      <div>
-        <a href='https://claytoncountyga.gov'>
-          <img src={clayconn} alt='' />
-        </a>
-      </div>
-      <div>
-        <p>Clayton County Permitting</p>
-      </div>
-    </div>
-  )
-}
-
-
-
-//<img src={logo} className="App-logo" alt="logo" />
