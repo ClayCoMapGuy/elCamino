af93131c892bf53b41bda08e634d54840174cc36
JonathonCG <Jonathon.gossett@claytoncountyga.gov>
2022-07-18 12:11:00 -0400

    Added tree end properties
    added properties to find and display the end of the decision tree

--- src/Navigation/QuestionCardsInfo.js
+++ src/Navigation/QuestionCardsInfo.js
@@ -2,36 +2,66 @@
 
 
 export const Questions = [
+    
     {questionText : 'Building Permit', 
     id : 0, 
-    nextPage : 1},
+    nextPage : 1,
+    link : 'claytoncountyga.gov',
+    lastItem : false},
+
     {questionText : 'Business License', 
     id : 1, 
-    nextPage : 1},
+    nextPage : 1,
+    link : 'claytoncountyga.gov',
+    lastItem : false},
+
     {questionText : 'Alcohal License Renewal/Application', 
     id : 2, 
-    nextPage : 1},
+    nextPage : 1,
+    link : 'claytoncountyga.gov',
+    lastItem : false},
+
     {questionText : 'For a Residential Property', 
     id : 3, 
-    nextPage : 2},
+    nextPage : 2,
+    link : 'claytoncountyga.gov',
+    lastItem : false},
+
     {questionText : 'For a Commerical Property', 
     id : 4, 
-    nextPage : 3},
+    nextPage : 3,
+    link : 'claytoncountyga.gov',
+    lastItem : false},
+
     {questionText : 'A new Permit', 
     id : 5, 
-    nextPage : 3},
+    nextPage : 3
+    ,link : 'claytoncountyga.gov',
+    lastItem : false},
+
     {questionText : 'Renewal of an existing permit', 
     id : 6, 
-    nextPage : 3},
+    nextPage : 3,
+    link : 'claytoncountyga.gov',
+    lastItem : false},
+
     {questionText : 'I want to add a deck', 
     id : 7, 
-    nextPage : 0},
+    nextPage : 0,
+    link : 'claytoncountyga.gov',
+    lastItem : true},
+
     {questionText : 'I want to add a room', 
     id : 8, 
-    nextPage : 0},
+    nextPage : 0,
+    link : 'claytoncountyga.gov',
+    lastItem : true},
+
     {questionText : 'I want to add a permanent carport', 
     id : 9, 
-    nextPage : 4},
+    nextPage : 4,
+    link : 'claytoncountyga.gov',
+    lastItem : false},
     ];
 
     //each array is a 'group' of cards displayed on screen at one time, values are the index of the 'Questions' array above.
