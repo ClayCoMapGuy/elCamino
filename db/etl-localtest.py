814f62e647ef68b68e5f20252d408dad1c5cc623
ClayCoMapGuy <109229572+ClayCoMapGuy@users.noreply.github.com>
2022-09-09 14:21:50 -0400

    QGIS ETL

--- db/etl-localtest.py
+++ db/etl-localtest.py
@@ -1,6 +1,6 @@
 """
 Model exported as python.
-Name : elCamino Tool
+Name : elCamino Tool Test
 Group : 
 With QGIS : 32600
 """
@@ -12,7 +12,7 @@ from qgis.core import QgsCoordinateReferenceSystem
 import processing
 
 
-class ElcaminoTool(QgsProcessingAlgorithm):
+class ElcaminoToolTest(QgsProcessingAlgorithm):
 
     def initAlgorithm(self, config=None):
         pass
@@ -20,7 +20,7 @@ class ElcaminoTool(QgsProcessingAlgorithm):
     def processAlgorithm(self, parameters, context, model_feedback):
         # Use a multi-step feedback, so that individual child algorithm progress reports are adjusted for the
         # overall progress through the model
-        feedback = QgsProcessingMultiStepFeedback(8, model_feedback)
+        feedback = QgsProcessingMultiStepFeedback(7, model_feedback)
         results = {}
         outputs = {}
 
@@ -53,13 +53,7 @@ class ElcaminoTool(QgsProcessingAlgorithm):
             return {}
 
         # PiP Query
-        # This is basically a Point in Polygon Query that: 
-        # 1. Picks all the APs that lie within a city boundary;
-        # 2. Creates two datasets:
-		#	a. APs inside city limits,
-		#	b. APs outside city limits;
-        # 3. In dataset (a), a new field is given called 'WITHINNAME' that should hold the NAME of the city limits polygon it is WITHIN
-		#	note: dataset (b) is just a collection of the APs that are outside cities, this algorithm does nothing else to the data
+        # Point in Polygon Query, APs (point) in Municipalities (poly)
         alg_params = {
             'DISCARD_NONMATCHING': True,
             'INPUT': outputs['ReprojectAps']['OUTPUT'],
@@ -123,6 +117,7 @@ class ElcaminoTool(QgsProcessingAlgorithm):
             'GROUP_FIELDS': [''],
             'INPUT': outputs['DropMzValues']['OUTPUT'],
             'MODULUS': 0,
+            'OUTPUT': 'C:/Users/robert.zimmerman/Downloads/test.shp',
             'SORT_ASCENDING': True,
             'SORT_EXPRESSION': '',
             'SORT_NULLS_FIRST': False,
@@ -130,34 +125,13 @@ class ElcaminoTool(QgsProcessingAlgorithm):
             'OUTPUT': QgsProcessing.TEMPORARY_OUTPUT
         }
         outputs['AddFid'] = processing.run('native:addautoincrementalfield', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-
-        feedback.setCurrentStep(7)
-        if feedback.isCanceled():
-            return {}
-
-        # Export to PostgreSQL
-        alg_params = {
-            'CREATEINDEX': True,
-            'DATABASE': 'doitwebdev01',
-            'DROP_STRING_LENGTH': False,
-            'ENCODING': 'UTF-8',
-            'FORCE_SINGLEPART': False,
-            'GEOMETRY_COLUMN': 'geom',
-            'INPUT': outputs['AddFid']['OUTPUT'],
-            'LOWERCASE_NAMES': True,
-            'OVERWRITE': True,
-            'PRIMARY_KEY': 'fid',
-            'SCHEMA': 'public',
-            'TABLENAME': 'addresses'
-        }
-        outputs['ExportToPostgresql'] = processing.run('native:importintopostgis', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
         return results
 
     def name(self):
-        return 'elCamino Tool'
+        return 'elCamino Tool Test'
 
     def displayName(self):
-        return 'elCamino Tool'
+        return 'elCamino Tool Test'
 
     def group(self):
         return ''
@@ -166,4 +140,4 @@ class ElcaminoTool(QgsProcessingAlgorithm):
         return ''
 
     def createInstance(self):
-        return ElcaminoTool()
+        return ElcaminoToolTest()
