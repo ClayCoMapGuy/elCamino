6e4493d6e447409ff3a909a3163cf95523ed3da3
JonathonCG <107061232+JonathonCG@users.noreply.github.com>
2022-09-29 13:34:33 -0400

    servertesting
    node api server testing

--- web/src/App.js
+++ web/src/App.js
@@ -1,10 +1,12 @@
 //import logo from './logo.svg';
 import './App.css';
 import { Cards } from './Navigation/QuestionCards'
+import Search from './Navigation/Searchbar'
 
 export function App() {
   return (
     <div className="App">
+      <Search />
       <Cards />
     </div>
   );
