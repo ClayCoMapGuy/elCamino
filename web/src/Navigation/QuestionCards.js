642b99895b00ad6cd364e54e16f5669beebbbace
JonathonCG <107061232+JonathonCG@users.noreply.github.com>
2022-09-29 12:41:33 -0400

    Card additions
    Added images icon properties
    Added chevrons
    Removed css styling boxes

--- web/src/Navigation/QuestionCards.js
+++ web/src/Navigation/QuestionCards.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { Questions, QuestionGroups } from './QuestionCardsInfo'
-
+import rightchevron from '../Graphics/32rightchevron.png';
 
 //updates the cards displayed on click by changing the array being passed into the Cards component.
 //it takes in the a value, set to id, and then 
@@ -13,10 +13,11 @@ import { Questions, QuestionGroups } from './QuestionCardsInfo'
 //Draws the question cards on the screen based on the QuestionGroups array in QuestionCardsInfo.js
 export function Cards() {
     const [value, setValue] = useState(0); //standard function component state changing 
-    
+    let crumbs = [1,2,3];
     return (
+        
         <div className='OptionGrid'>
-        <p>Page: {value + 1} of {QuestionGroups.length}</p>    
+        <p>Page: {value + 1} of {QuestionGroups.length}</p>
         <button onClick={()=>setValue(0)}>Back to start</button>
          <ul>{
             QuestionGroups[value].map(cardList=>( //Maps the QuestionGroups array
@@ -27,10 +28,11 @@ export function Cards() {
                     <button 
                         className="questionButtons" //CSS class
                         //onClick function that changes the state to the ID of the question displayed.
-                        onClick={()=>setValue((Questions[cardList].nextPage))}> 
-                        <div className='Button_inline_image'>Placeholder for image</div>
-                        <div className='Button_inline_text'>{Questions[cardList].questionText} - to page {Questions[cardList].nextPage + 1}</div>
-                        <div className='Button_inline_icon'>Placeholder for arrow image</div>
+                        onClick={()=>setValue((Questions[cardList].nextPage))}>
+                        
+                        <div className='Button_inline_image'><img src={Questions[cardList].icon}></img></div>
+                        <div className='Button_inline_text'>{Questions[cardList].questionText} {/*- to page {Questions[cardList].nextPage + 1}*/}</div>
+                        <div className='Button_inline_icon'><img src={rightchevron}></img></div>
                     </button>
                 </li>
                 ))
